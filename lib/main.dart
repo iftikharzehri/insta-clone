8d7317abc61ec5ca81984b0ab9d0602155305396
iftikharzehri <iftikharahmed111000@gmail.com>
2023-07-07 15:37:43 +0500

    making home class contents

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,20 @@ class MyApp extends StatelessWidget {
 }
 
 // ignore: camel_case_types
-class homeInsta extends StatelessWidget {
+class homeInsta extends StatefulWidget {
   const homeInsta({super.key});
+
+  @override
+  State<homeInsta> createState() => _homeInstaState();
+}
+
+class _homeInstaState extends State<homeInsta> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,115 +47,141 @@ class homeInsta extends StatelessWidget {
           ),
           bottom: TabBar(
             isScrollable: true,
-            indicator: BoxDecoration(
-              borderRadius: BorderRadius.circular(80),
-              color: Colors.blue,
-            ),
+            indicator: null,
+            // indicator: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(25),
+            //   color: Colors.blue,
+            // ),
             tabs: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  // radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  // radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: CircleAvatar(
-                  radius: 25,
-                  child: ClipOval(
-                    child: Image.asset(
-                      "images/a.png",
-                      fit: BoxFit.cover,
-                    ),
+              CircleAvatar(
+                radius: 25,
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/a.png",
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
-              // Icon(Icons.abc),
             ],
           ),
 
           actions: const <Widget>[
-            Icon(Icons.heart_broken),
-            Icon(Icons.message),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 1,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white,
-          backgroundColor: Colors.black,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "home",
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 25.0, top: 8),
+                  child: Icon(Icons.favorite_border),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 25, top: 8),
+                  child: Icon(Icons.maps_ugc),
+                )
+              ],
             ),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "search"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile")
           ],
         ),
-        body: const TabBarView(
-          children: [
-            homeclass(),
-            searchClass(),
-            searchClass(),
-            searchClass(),
-            searchClass(),
-            searchClass(),
-          ],
+        // body: const TabBarView(
+        //   children: [
+        //     homeclass(),
+        //     searchClass(),
+        //     searchClass(),
+        //     searchClass(),
+        //     searchClass(),
+        //     searchClass(),
+        //   ],
+        // ),
+        bottomNavigationBar: Theme(
+          data: ThemeData(canvasColor: Colors.black),
+          child: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            unselectedItemColor: Colors.white,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: 30,
+                ),
+                label: "home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search_outlined,
+                  size: 30,
+                ),
+                label: "search",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_box_outlined,
+                  size: 30,
+                ),
+                label: "post",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.video_call,
+                  size: 30,
+                ),
+                label: "reels",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+                label: "profile",
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.white,
+            onTap: _onItemTapped,
+          ),
         ),
       ),
     );
