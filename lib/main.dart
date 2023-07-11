4b71c7b68af7cdbe7ff9ca8761ba15e959f1b247
iftikharzehri <iftikharahmed111000@gmail.com>
2023-07-11 16:45:29 +0500

    stuck on tabbar size

--- lib/main.dart
+++ lib/main.dart
@@ -116,37 +116,36 @@ class _homeclassState extends State<homeclass> {
   List profileImages = [
     "https://cdn.pixabay.com/photo/2023/07/04/17/28/red-squirrel-8106699_1280.jpg",
     "https://cdn.pixabay.com/photo/2017/08/06/12/06/people-2591874_1280.jpg",
-    "https://cdn.pixabay.com/photo/2016/11/14/03/06/woman-1822459_1280.jpg",
-    "https://cdn.pixabay.com/photo/2015/06/22/08/39/child-817371_1280.jpg",
-    "https://cdn.pixabay.com/photo/2016/11/08/05/31/boys-1807545_1280.jpg",
+    // "https://cdn.pixabay.com/photo/2016/11/14/03/06/woman-1822459_1280.jpg",
     "https://cdn.pixabay.com/photo/2017/12/31/15/56/portrait-3052641_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/11/08/05/31/boys-1807545_1280.jpg",
     "https://cdn.pixabay.com/photo/2013/02/21/19/10/mother-84628_1280.jpg",
-    "https://cdn.pixabay.com/photo/2018/02/21/08/40/woman-3169726_1280.jpg",
     "https://cdn.pixabay.com/photo/2018/04/05/09/32/portrait-3292287_1280.jpg",
-    // " https://cdn.pixabay.com/photo/2017/08/05/14/21/love-2583943_1280.jpg",
+    "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
+    "https://cdn.pixabay.com/photo/2014/06/18/13/44/emotions-371238_1280.jpg"
   ];
   List imagepaths = [
-    // "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    "https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396_1280.jpg",
     "https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_960_720.jpg",
     "https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_960_720.jpg",
     "https://cdn.pixabay.com/photo/2014/09/14/18/04/dandelion-445228_960_720.jpg",
     "https://cdn.pixabay.com/photo/2023/07/04/17/28/red-squirrel-8106699_1280.jpg",
     "https://cdn.pixabay.com/photo/2016/07/11/15/43/pretty-woman-1509956_960_720.jpg",
     "https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2013/11/28/10/03/autumn-219972_960_720.jpg",
-    "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_960_720.jpg",
+    // "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
+    // "https://cdn.pixabay.com/photo/2013/11/28/10/03/autumn-219972_960_720.jpg",
+    // "https://cdn.pixabay.com/photo/2017/12/17/19/08/away-3024773_960_720.jpg",
   ];
   List<String> userNames = [
     "iftikhar zehri",
     "Rehmal zagrain",
     "fazal karim",
-    "john Elia",
+    // "john Elia",
     "Aslam baloch",
     "Rehman jan",
     "Muslim Shafi",
     "katrine",
-    "Mark",
+    // "Mark",
     // "elon musk",
   ];
 
@@ -180,26 +179,30 @@ class _homeclassState extends State<homeclass> {
           bottom: TabBar(
             isScrollable: true,
             indicator: null,
-            padding: const EdgeInsets.all(1),
+            // labelPadding: EdgeInsets.symmetric(horizontal: 16),
+            // padding: const EdgeInsets.all(1),
             tabs: [
               for (int i = 0; i < profileImages.length; i++)
                 Tab(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 35,
-                          child: ClipOval(
-                            child: Image.network(
-                              profileImages[i],
-                              fit: BoxFit.cover,
-                              height: 100,
-                              width: 100,
+                  child: SizedBox(
+                    height: 120,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 35,
+                            child: ClipOval(
+                              child: Image.network(
+                                profileImages[i],
+                                fit: BoxFit.fitWidth,
+                                height: 100,
+                                width: 100,
+                              ),
                             ),
                           ),
-                        ),
-                        const Text("data1"),
-                      ],
+                          Text("data1"),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -224,100 +227,96 @@ class _homeclassState extends State<homeclass> {
         //   },
         //   itemCount: imagepaths.length,
         // ),
-        resizeToAvoidBottomInset: false,
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.black,
-                    height: 640,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 25,
-                                child: ClipOval(
-                                  child: Image.network(
-                                    profileImages[index],
-                                    height: 100,
-                                    width: 100,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        userNames[index],
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Icon(
-                                  Icons.more_vert,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Image.network(
-                            imagepaths[index],
-                          ),
-                        ),
-                        const Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(Icons.favorite_outline,
-                                  color: Colors.white),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(
-                                Icons.mode_comment_outlined,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(Icons.send, color: Colors.white),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.all(8),
-                                child:
-                                    Icon(Icons.more_vert, color: Colors.white),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+
+        // body: ListView.builder(
+        //   itemBuilder: (BuildContext context, int index) {
+        //     return Column(
+        //       children: [
+        //         Container(
+        //           color: Colors.black,
+        //           height: 600,
+        //           child: Column(
+        //             children: [
+        //               Padding(
+        //                 padding: EdgeInsets.only(left: 8, right: 8),
+        //                 child: Row(
+        //                   children: [
+        //                     CircleAvatar(
+        //                       radius: 25,
+        //                       child: ClipOval(
+        //                         child: Image.network(
+        //                           profileImages[index],
+        //                           height: 100,
+        //                           width: 100,
+        //                           fit: BoxFit.cover,
+        //                         ),
+        //                       ),
+        //                     ),
+        //                     Expanded(
+        //                       child: Padding(
+        //                         padding: EdgeInsets.all(8),
+        //                         child: Column(
+        //                           crossAxisAlignment: CrossAxisAlignment.start,
+        //                           children: [
+        //                             Text(
+        //                               userNames[index],
+        //                               style: const TextStyle(
+        //                                 color: Colors.white,
+        //                                 fontWeight: FontWeight.bold,
+        //                               ),
+        //                             ),
+        //                           ],
+        //                         ),
+        //                       ),
+        //                     ),
+        //                     const Padding(
+        //                       padding: EdgeInsets.all(8),
+        //                       child: Icon(
+        //                         Icons.more_vert,
+        //                         color: Colors.white,
+        //                       ),
+        //                     ),
+        //                   ],
+        //                 ),
+        //               ),
+        //               Expanded(
+        //                 child: Image.network(
+        //                   imagepaths[index],
+        //                 ),
+        //               ),
+        //               const Row(
+        //                 children: [
+        //                   Padding(
+        //                     padding: EdgeInsets.all(8.0),
+        //                     child: Icon(Icons.favorite_outline,
+        //                         color: Colors.white),
+        //                   ),
+        //                   Padding(
+        //                     padding: EdgeInsets.all(8.0),
+        //                     child: Icon(
+        //                       Icons.mode_comment_outlined,
+        //                       color: Colors.white,
+        //                     ),
+        //                   ),
+        //                   Padding(
+        //                     padding: EdgeInsets.all(8.0),
+        //                     child: Icon(Icons.send, color: Colors.white),
+        //                   ),
+        //                   Expanded(
+        //                     child: Padding(
+        //                       padding: EdgeInsets.all(8),
+        //                       child: Icon(Icons.more_vert, color: Colors.white),
+        //                     ),
+        //                   ),
+        //                 ],
+        //               ),
+        //             ],
+        //           ),
+        //         ),
+        //       ],
+        //     );
+        //   },
+        // ),
       ),
     );
   }
